00d0930f7b4219cd02e784869d9803f1cda16f62
EvgeniiyaR <tulpansik@mail.ru>
2023-04-18 01:36:26 +0300

    feat: resolve tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
+  let tmpList = [];
+  let resultList = [];
+  let additionsList = [];
+  if (typeof(options['repeatTimes']) === "undefined") {
+    options.repeatTimes = 1;
+  };
+  if (typeof(options['separator']) === "undefined") {
+    options.separator = '+';
+  };
+  if (typeof(options['addition']) === "undefined") {
+    options.addition = '';
+  };
+  if (typeof(options['additionRepeatTimes']) === "undefined") {
+    options.additionRepeatTimes = 1;
+  };
+  if (typeof(options['additionSeparator']) === "undefined") {
+    options.additionSeparator = '|';
+  };
+  for (let i = 0; i < options.repeatTimes; i++) {
+    tmpList.push(str);
+  }
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionsList.push(String(options.addition));
+  }
+  for (let i = 0; i < tmpList.length; i++) {
+    resultList.push(tmpList[i] + additionsList.join(options.additionSeparator));
+  }
+
+  return resultList.join(options.separator);
+
 }
 
 module.exports = {
